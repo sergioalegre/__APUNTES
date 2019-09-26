c1eb23584557615728134939ca49e5ad2fdb4e2c
Sergio Alegre <sergio.alegre.arribas@gmail.com>
2019-09-26 08:49:49 +0200

    Create depurado.php

---
+++ depurado.php
@@ -0,0 +1,20 @@
+<?php 
+
+	 //vuelca lo que recibe por POST de otra página
+	var_dump($_POST); 
+
+
+
+	//volcado de una consulta sql
+	$sql = "INSERT INTO PUBLICACIONES (titulo, descripcion, orden, es, en, fr) VALUES (:titulo, :descripcion, :orden, :es, :en, :fr)";
+    $data = array(
+        ":titulo"=>$_POST["titulo"],
+        ":descripcion"=>$_POST["descripcion"],
+        ":orden"=>$maximo_orden["MAXIMO"] +1,
+        ":es"=>$es,
+        ":en"=>$en,
+        ":fr"=>$fr
+    );
+	var_dump($data);
+  
+ ?>
