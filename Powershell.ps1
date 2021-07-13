b6a72eee72a8a127a67512b31c6fa5720db065c0
SergioAlegre <sergio.alegre.arribas@gmail.com>
2021-07-13 13:15:53 +0200

    ++powershell

---
+++ Powershell.ps1
@@ -0,0 +1,17 @@
+#Convertir de string a datetime
+PowerCLI C:\> $a.GetType()
+
+IsPublic IsSerial Name                                     BaseType
+-------- -------- ----                                     --------
+True     True     String                                   System.Object
+
+
+PowerCLI C:\> $a=[datetime]::parseexact($a,"dd'/'MM'/'yyyy hh:mm:ss", $null)
+PowerCLI C:\> $a.GetType()
+
+IsPublic IsSerial Name                                     BaseType
+-------- -------- ----                                     --------
+True     True     DateTime                                 System.ValueType
+
+#Convertir a String
+$limite.ToString()
